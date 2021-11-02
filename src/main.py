da4e9f8db7b4fcda802fdf941bc7c6cfa536a938
Oswaldo <salasoswaldoa@gmail.com>
2021-11-02 16:36:06 -0400

    se agrego endpoint para registrar ong

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import Favorite, db, User
+from models import  db, Ong
 import requests
 from flask_jwt_extended import JWTManager,create_access_token, get_jwt_identity,jwt_required
 
@@ -48,145 +48,163 @@ def sitemap():
 
 
 
-# crea un nuevo usuario
-@app.route('/users', methods=['POST'])
+# registra una ONG
+@app.route('/teapoyo', methods=['POST'])
 
-def handle_create_users():
-    data_new_user = request.json
-    new_user=User.create(data_new_user)
-    if new_user is not None:
-        return jsonify(new_user), 200
-    return jsonify({"message":"something happened,try again!"}),400
-
-
-   
-# retorna los datos del usuario
-@app.route('/users/<int:user_id>' , methods=['GET'])
-
-def handle_data_user(user_id):
-    response= User.query.filter_by(id= user_id).one_or_none()
-    if response is not None:
-        return jsonify(response.serialize()),200
-    else:
-        return jsonify({"message":"Not founded"}),401
+def handle_registration_ong():
+    data_new_ong = request.json
+    
+    new_ong=Ong.create(data_new_ong)
+    if new_ong is None:
+        return jsonify({"message": False}),400
+    return jsonify(new_ong), 200
 
 
-
-# login,  retorna un token del usuario
+# login,  retorna un token de la ONG
 
 @app.route('/login',  methods=['POST'])
 
-def handle_user_login():
-    user_name= request.json.get('user_name', None)
+def handle_ong_login():
+    ong_name= request.json.get('ong_name', None)
     password= request.json.get('password',None)
-    print(user_name,password)
-    user= User.query.filter_by(username=user_name,password= password).one_or_none()
-    if user is not None:
-        access_token= create_access_token(identity=user.id)
-        return jsonify({"token":access_token, "user_id":user.id,"user_name":user.username}),200
+    ong= Ong.query.filter_by(ong_name=ong_name,password= password).one_or_none()
+    if ong is not None:
+        access_token= create_access_token(identity=ong.id)
+        return jsonify({"token":access_token, "ong_name":ong.ong_name}),200
     else:
         return jsonify({"message":"Credentials invalid"}),401
 
 
 
 
-#retorna todos los recursos según la naturaleza especificada
-@app.route('/<string:nature>', methods=['GET'])
 
-def handle_recourse(nature):
-    # las variables determinan el numero de recursos a solicitar 
-    # 
-    limit= request.args.get('limit',100)
-    page=request.args.get('page',1)
-    # solicitud a swapi de los recursos 
-    response= requests.get(f'https://www.swapi.tech/api/{nature}?page={page}&limit={limit}')
-    body=response.json()
-    #convierte las url de response en url de mi api
-    for result in body['results']:
-        result['url']= swapi_to_localhost(result['url'])
+   
+# # retorna los datos del usuario
+# @app.route('/users/<int:user_id>' , methods=['GET'])
+
+# def handle_data_user(user_id):
+#     response= User.query.filter_by(id= user_id).one_or_none()
+#     if response is not None:
+#         return jsonify(response.serialize()),200
+#     else:
+#         return jsonify({"message":"Not founded"}),401
+
+
+
+# # login,  retorna un token del usuario
+
+# @app.route('/login',  methods=['POST'])
+
+# def handle_user_login():
+#     user_name= request.json.get('user_name', None)
+#     password= request.json.get('password',None)
+#     user= User.query.filter_by(username=user_name,password= password).one_or_none()
+#     if user is not None:
+#         access_token= create_access_token(identity=user.id)
+#         return jsonify({"token":access_token, "user_id":user.id,"user_name":user.username}),200
+#     else:
+#         return jsonify({"message":"Credentials invalid"}),401
+
+
+
+
+# #retorna todos los recursos según la naturaleza especificada
+# @app.route('/<string:nature>', methods=['GET'])
+
+# def handle_recourse(nature):
+#     # las variables determinan el numero de recursos a solicitar 
+#     # 
+#     limit= request.args.get('limit',100)
+#     page=request.args.get('page',1)
+#     # solicitud a swapi de los recursos 
+#     response= requests.get(f'https://www.swapi.tech/api/{nature}?page={page}&limit={limit}')
+#     body=response.json()
+#     #convierte las url de response en url de mi api
+#     for result in body['results']:
+#         result['url']= swapi_to_localhost(result['url'])
      
-     #condición para enviar todo response o solo los diccionarios de response
+#      #condición para enviar todo response o solo los diccionarios de response
 
-    if body['previous']== None and body['next']== None:
-        return jsonify(body['results']),response.status_code
-    else:
-        body.update(
-        previous = swapi_to_localhost(body['previous']) if body['previous'] else None,
-        next= swapi_to_localhost(body['next']) if body['next'] else None,
-        )
-        return jsonify(body),response.status_code
-
-
-#retorna un recursos por su id
-
-@app.route('/<string:nature>/<int:nature_id>', methods=['GET'])
-
-def handle_one_recourse(nature, nature_id):
-    # solicitud a swapi de un recursos nature/id
-    response= requests.get(f'https://www.swapi.tech/api/{nature}/{nature_id}')
-    body= response.json()
-
-    if response.status_code== 200:
-        result=body['result']
-        #convierte las url de response en url de mi api
-        result['properties'].update(
-            url=swapi_to_localhost(result['properties']['url']),
-        )
-        # si existe la propiedad homeworld entonces cambia la url de homeword por url de mi api
-        if 'homeworld' in result['properties']:
-            result['properties'].update(
-            homeworld= swapi_to_localhost(result['properties']['homeworld']) 
-            )
-
-        return jsonify(result),response.status_code
-    else:
-        return jsonify(body),response.status_code
+#     if body['previous']== None and body['next']== None:
+#         return jsonify(body['results']),response.status_code
+#     else:
+#         body.update(
+#         previous = swapi_to_localhost(body['previous']) if body['previous'] else None,
+#         next= swapi_to_localhost(body['next']) if body['next'] else None,
+#         )
+#         return jsonify(body),response.status_code
 
 
+# #retorna un recursos por su id
 
-#Con GET se obtiene todos los favoritos del usuario
+# @app.route('/<string:nature>/<int:nature_id>', methods=['GET'])
 
-@app.route('/favorites', methods=['GET'])
-@jwt_required()
-def handle_favorites():
-        user= get_jwt_identity()
-        favorites= Favorite.query.filter_by(user_id = get_jwt_identity())
-        response=list(map(lambda favorite:favorite.serialize(),favorites))
-       
-        return jsonify(response),200    
+# def handle_one_recourse(nature, nature_id):
+#     # solicitud a swapi de un recursos nature/id
+#     response= requests.get(f'https://www.swapi.tech/api/{nature}/{nature_id}')
+#     body= response.json()
 
-    
-@app.route('/favorites/add', methods=['POST'])
-@jwt_required()
-def handle_favorites_add():
-        data_favorite= request.json
-        user_id= get_jwt_identity()
-        data={
-           'url':data_favorite['url'],
-           'name_favorite':data_favorite['name_favorite'],
-           'user_id':user_id
-           
-       }
+#     if response.status_code== 200:
+#         result=body['result']
+#         #convierte las url de response en url de mi api
+#         result['properties'].update(
+#             url=swapi_to_localhost(result['properties']['url']),
+#         )
+#         # si existe la propiedad homeworld entonces cambia la url de homeword por url de mi api
+#         if 'homeworld' in result['properties']:
+#             result['properties'].update(
+#             homeworld= swapi_to_localhost(result['properties']['homeworld']) 
+#             )
 
-        response=Favorite.create(data)
-        if response:
-            return jsonify(response),201
-        else:
-            return jsonify( None),400
+#         return jsonify(result),response.status_code
+#     else:
+#         return jsonify(body),response.status_code
 
 
 
-@app.route('/favorites/delete/<int:favorite_id>', methods=[ 'DELETE'])
+# #Con GET se obtiene todos los favoritos del usuario
+
+# @app.route('/favorites', methods=['GET'])
 # @jwt_required()
-def handle_delete_favorites(favorite_id):
+# def handle_favorites():
+#         user= get_jwt_identity()
+#         favorites= Favorite.query.filter_by(user_id = get_jwt_identity())
+#         response=list(map(lambda favorite:favorite.serialize(),favorites))
+       
+#         return jsonify(response),200    
 
-    favorite= Favorite.query.filter_by(id = favorite_id).one_or_none()
     
-    if favorite:
-        favorite.delete()
-        return jsonify({"message":"Done"}),200    
-    else: 
-        return jsonify({"message":"Not found"}),401
+# @app.route('/favorites/add', methods=['POST'])
+# @jwt_required()
+# def handle_favorites_add():
+#         data_favorite= request.json
+#         user_id= get_jwt_identity()
+#         data={
+#            'url':data_favorite['url'],
+#            'name_favorite':data_favorite['name_favorite'],
+#            'user_id':user_id
+           
+#        }
+
+#         response=Favorite.create(data)
+#         if response:
+#             return jsonify(response),201
+#         else:
+#             return jsonify( None),400
+
+
+
+# @app.route('/favorites/delete/<int:favorite_id>', methods=[ 'DELETE'])
+# # @jwt_required()
+# def handle_delete_favorites(favorite_id):
+
+#     favorite= Favorite.query.filter_by(id = favorite_id).one_or_none()
+    
+#     if favorite:
+#         favorite.delete()
+#         return jsonify({"message":"Done"}),200    
+#     else: 
+#         return jsonify({"message":"Not found"}),401
 
 
 
--- src/models.py
+++ src/models.py
@@ -1,27 +1,37 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from requests.api import delete
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Ong(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    favorites = db.relationship('Favorite', backref='user',uselist=True)
+    ong_name = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    location = db.Column(db.String(100), unique= False, nullable= False)
+    image= db.Column(db.String(100), unique = False, nullable= False)
+    logo = db.Column(db.String(100),unique = False, nullable= False)
+    website_address = db.Column(db.String(50),unique= False, nullable =False)
+    rif= db.Column(db.String(20),unique= False, nullable = False)
+    description= db.Column(db.String(300),unique=False, nullable=True)
+    # activities = db.relationship('Activity', backref='org',uselist=True)
     
     def serialize(self):
         return{
             'id':self.id,
-            'username':self.username,
-            
-            'favorites':[favorite.serialize() for favorite in self.favorites]
+            'ong_name':self.ong_name,
+            'location': self.location,
+            'logo': self.logo,
+            'rif': self.rif,
+            'website_address':self.website_address,
+            # 'activities':[activity.serialize() for activity in self.activities]
         }
 
     @classmethod
-    def create(cls,data_new_user):
-        new_user= cls(**data_new_user)
+    def create(cls,data_new_ong):
+        new_ong= cls(**data_new_ong)
         try:
-            db.session.add(new_user)
+            db.session.add(new_ong)
             db.session.commit()
             return({"message":"done"})
         except Exception as error:
@@ -31,44 +41,44 @@ class User(db.Model):
 
   
 
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    url= db.Column(db.String(120),nullable= False)
-    name_favorite= db.Column(db.String(30),nullable= False)
-    user_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    __table_args__=(db.UniqueConstraint(
-        'user_id',
-        'url',
-        name='unique_fav_for_user'
+# class Favorite(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     url= db.Column(db.String(120),nullable= False)
+#     name_favorite= db.Column(db.String(30),nullable= False)
+#     user_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+#     __table_args__=(db.UniqueConstraint(
+#         'user_id',
+#         'url',
+#         name='unique_fav_for_user'
         
-    ),)
+#     ),)
 
-    def serialize(self):
-        return{
-            'id':self.id,
-            'user_id':self.user_id,
-            'name_favorite': self.name_favorite,
-            'url':self.url,
-        }
+#     def serialize(self):
+#         return{
+#             'id':self.id,
+#             'user_id':self.user_id,
+#             'name_favorite': self.name_favorite,
+#             'url':self.url,
+#         }
     
-    def delete(self):
-        db.session.delete(self)
-        try:
-            db.session.commit()
-            return True
-        except Exception as error:
-            db.session.rollback
-            return None
+#     def delete(self):
+#         db.session.delete(self)
+#         try:
+#             db.session.commit()
+#             return True
+#         except Exception as error:
+#             db.session.rollback
+#             return None
 
-    @classmethod
-    def create(cls,data_favorite):
-        new_favorite= cls(**data_favorite)
-        try:
-            db.session.add(new_favorite)
-            db.session.commit()
-            return(new_favorite.serialize())
-        except Exception as error:
-            db.session.rollback()
-            print(error)
-            return None
+#     @classmethod
+#     def create(cls,data_favorite):
+#         new_favorite= cls(**data_favorite)
+#         try:
+#             db.session.add(new_favorite)
+#             db.session.commit()
+#             return(new_favorite.serialize())
+#         except Exception as error:
+#             db.session.rollback()
+#             print(error)
+#             return None
         
